1fdbfcdabe390296e1bdf9758d6690cbc31d3f55
malubitencourt <bitencourt496@gmail.com>
2022-03-27 14:49:44 -0300

    conteudos e exercicios da aula de modules

---
+++ 02-modules/animacoes.js
@@ -0,0 +1,93 @@
+//atualizado para dataset para deixar a documentação mais compreesível
+function initTabNav() { //função para isolar os codigos e evitar possiveis conflitos no Js
+    const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
+    const tabContent = document.querySelectorAll('[data-tab="content"] section');
+    const activeClass = 'ativo';
+
+    if(tabMenu.length && tabContent.length) { //verifica se existe itens nessa nas constantes selecionadas para depois passar os codigo dentro da verificação.
+
+        tabContent[0].classList.add(activeClass) //adiciona a classe ativo sempre no primeiro item da lista.
+
+        // função que adiciona a classe ativo no elemento selecionado e remove dos demais usando o loop.
+        function activeTab(index) { 
+            tabContent.forEach((content) => { 
+                content.classList.remove(activeClass); //loop que remove a classe das os outros elementos.
+            });
+            const direcao = tabContent[index].dataset.anime
+            tabContent[index].classList.add(activeClass, direcao); //adiciona a classe.
+        };
+
+        //loop que adiciona um evento de 'click' em todos os itens que quando ocorrer  retornara a função anterior usando o index do item clicado. 
+        tabMenu.forEach((itemMenu, index) => { 
+            itemMenu.addEventListener('click', () => {
+                activeTab(index);
+            } )
+        });
+    };
+}
+initTabNav();
+
+
+function initAccordion(){
+    const accordionList = document.querySelectorAll('[data-anime="accordion"] dt');
+    const activeClass = 'ativo';
+    accordionList[0].classList.add(activeClass);
+    accordionList[0].nextElementSibling.classList.add(activeClass);
+
+    function activeAccordion(event) {
+        this.classList.toggle(activeClass);
+        this.nextElementSibling.classList.toggle(activeClass);
+    };
+
+
+    accordionList.forEach((item) => {
+    item.addEventListener('click', activeAccordion);
+    });
+}
+initAccordion();
+
+
+function initScrollSuave(){
+    const linksInternos = document.querySelectorAll('[data-menu="suave"] a[href^="#"]');
+
+    function scrollToSection(event) {
+    event.preventDefault();
+        const href = event.currentTarget.getAttribute('href');
+        const section = document.querySelector(href);
+        section.scrollIntoView({
+            behavior:'smooth',
+            block: 'start',
+  }); 
+
+    //forma alternativa
+        // const topo = section.offsetTop;
+        // window.scrollTo({
+        //     top: topo,
+        //     behavior:'smooth',
+        // });
+    }
+
+    linksInternos.forEach((link) =>{
+        link.addEventListener('click', scrollToSection);
+    });
+};
+initScrollSuave();
+
+function initAnimaScroll(){ 
+    const sections = document.querySelectorAll('[data-anime="scroll"]');
+    if(sections.length) {
+    const windowMetade = window.innerHeight * 0.6;
+    function animaScroll(){
+        sections.forEach((section) => {
+            const sectionTop = section.getBoundingClientRect().top;
+            const isSectionVisible = (sectionTop - windowMetade) < 0;
+            if(isSectionVisible) {
+              section.classList.add('ativo');
+            };
+        })    
+    };
+    animaScroll();
+    window.addEventListener('scroll', animaScroll);
+};
+};
+initAnimaScroll();
